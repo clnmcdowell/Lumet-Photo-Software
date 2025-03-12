531f90bf399a05441f08cfa0412bf8e6b8228a5a
clnmcdowell <clnmcdowell@gmail.com>
2025-03-12 14:21:26 -0400

    Create file dialogue to select folder

--- lumet_sorter.py
+++ lumet_sorter.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import filedialog
 
 root = tk.Tk()
 root.title('Lumet Sorter')
+
+folderPath = filedialog.askdirectory(title = "Select Folder of Photos to Sort")
+print("Selected Folder: " + folderPath)
